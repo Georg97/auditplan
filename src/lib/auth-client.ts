f4d8d0fadacbf9c3faf1f6a1c51985e811814d1c
Georg <75206704+Georg97@users.noreply.github.com>
2026-03-18 23:21:09 +0100

    fixed auth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,2 @@
-import "dotenv/config"
 import { createAuthClient } from "better-auth/svelte"
-export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.BETTER_AUTH_URL,
-})
+export const authClient = createAuthClient()
