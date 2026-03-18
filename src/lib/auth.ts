abb9200e86b5b93411c5956c5c0507dbc5c073ce
Georg <75206704+Georg97@users.noreply.github.com>
2026-03-18 22:29:22 +0100

    updated db setup to have auth + data in same database in drizzle

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,23 +1,24 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "../index"; // your drizzle instance
+import { db } from "$lib/server/db";
 import * as schema from "../db/schema";
 import { sveltekitCookies } from "better-auth/svelte-kit";
 import { getRequestEvent } from "$app/server";
+import { env } from "$env/dynamic/private";
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "sqlite",
         schema,
     }),
-    baseURL: process.env.BETTER_AUTH_URL,
+    baseURL: env.BETTER_AUTH_URL,
     emailAndPassword: {
         enabled: true,
     },
     socialProviders: {
         google: {
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            clientId: env.GOOGLE_CLIENT_ID as string,
+            clientSecret: env.GOOGLE_CLIENT_SECRET as string,
         },
     },
     plugins: [sveltekitCookies(getRequestEvent)], // make sure this is the last plugin in the array
